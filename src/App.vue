fcb51b1b2075a56cab6a96526ca54a51006bd0ba
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-22 14:35:25 +0200

    initial commit
    inizializzata app vite

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'Hello'
+    }
+  }
+
+}
+</script>
+
+<template>
+  <h1>
+    {{ message }}
+  </h1>
+
+  <HelloWorld />
+</template>
+
+<style></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default {
+  name: 'HelloWorld'
+}
+
+</script>
+
+<template>
+  <h1>
+    Hello world
+  </h1>
+</template>
+
+<style></style>
