bfe612125794fb975e915d931b516d1b16f738d8
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-25 14:34:10 +0200

    btn load more
    aggiunto bottone load more nnel main

--- src/components/AppMain2.vue
+++ src/components/AppMain2.vue
@@ -15,9 +15,9 @@ export default {
 <template>
   <div class="container w-75 py-4">
 
-    <h2>
+    <h4>
       <span class="badge bg-primary">CURRENT SERIES</span>
-    </h2>
+    </h4>
 
     <div class="row">
       <div class="col-4 col-md-3 col-lg-2 py-2" v-for="comic in comics">
@@ -28,6 +28,12 @@ export default {
           <h6>{{ comic.series }}</h6>
         </div>
       </div>
+    </div>
+
+    <div class="d-flex justify-content-center">
+      <button class="btn btn-primary fw-bold">
+        LOAD MORE
+      </button>
     </div>
 
   </div>
