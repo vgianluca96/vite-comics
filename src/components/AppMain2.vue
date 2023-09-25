38890a1d7a663ef645e8e1ba47f857802eef8100
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-25 12:58:08 +0200

    stampati fumetti
    usato v-for per stmpare i fumetti nel main

--- src/components/AppMain2.vue
+++ src/components/AppMain2.vue
@@ -1,14 +1,35 @@
 <script>
+import DCcomics from '../comics.json';
 
 export default {
-  name: 'AppMain2'
+  name: 'AppMain2',
+  data() {
+    return {
+      comics: DCcomics
+    }
+  }
 }
 
 </script>
 
 <template>
   <div class="container w-75 py-4">
-    <h1>content goes here</h1>
+
+    <h2>
+      <span class="badge bg-primary">CURRENT SERIES</span>
+    </h2>
+
+    <div class="row">
+      <div class="col-4 col-md-3 col-lg-2 py-2" v-for="comic in comics">
+        <div class="w-100 ratio ratio-1x1">
+          <img :src="comic.thumb" class="comic-img w-100" alt="...">
+        </div>
+        <div class="py-2 text-uppercase">
+          <h6>{{ comic.series }}</h6>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
